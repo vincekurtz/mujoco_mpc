7567ce78cafac9e7954ce2a643d521fc7331118c
Vince Kurtz <vjkurtz@gmail.com>
2024-12-03 13:04:23 -0600

    add mujoco and drake benchmarks

---
+++ mjpc/tasks/spinner/drake_forward_sim_test.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3.10
+
+import time
+
+from pydrake.multibody.parsing import Parser
+from pydrake.multibody.plant import AddMultibodyPlantSceneGraph
+from pydrake.systems.analysis import Simulator
+from pydrake.systems.framework import DiagramBuilder
+from pydrake.systems.primitives import RandomSource
+from pydrake.common import RandomDistribution
+
+import numpy as np
+
+builder = DiagramBuilder()
+plant, scene_graph = AddMultibodyPlantSceneGraph(
+    builder, time_step=0.05)
+parser = Parser(plant)
+
+parser.AddModels("spinner.xml")
+plant.Finalize()
+
+ctrl = builder.AddSystem(
+    RandomSource(num_outputs=2, 
+                 sampling_interval_sec=0.05,
+                 distribution=RandomDistribution.kGaussian)
+)
+
+builder.Connect(
+    ctrl.get_output_port(0),
+    plant.get_actuation_input_port()
+)
+    
+# Add visualization to see the geometries.
+diagram = builder.Build()
+
+num_trials = 10000
+times = []
+for i in range(num_trials):
+    simulator = Simulator(diagram)
+    simulator.set_target_realtime_rate(-1.)
+    simulator.Initialize()
+    st = time.time()
+    simulator.AdvanceTo(2.0)
+    times.append(time.time() - st)
+
+avg_time_ms = np.mean(times) * 1000
+std_time_ms = np.std(times) * 1000
+
+print(f"Average time: {avg_time_ms:.4f} +/- {std_time_ms:.4f} ms")
---
+++ mjpc/tasks/spinner/mujoco_forward_sim_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import time
+import mujoco
+import numpy as np
+
+# Load the model
+model = mujoco.MjModel.from_xml_path('task.xml')
+model.opt.timestep = 0.05
+data = mujoco.MjData(model)
+data.qpos = model.key("home").qpos
+
+def simulate():
+    """Simulate a random action sequence."""
+    U = np.random.normal(0, 1, (40, 2))
+    for u in U:
+        data.ctrl = u
+        mujoco.mj_step(model, data)
+    return data
+
+# Run the simulation
+num_trials = 10000
+times = []
+for i in range(num_trials):
+    data.qpos = model.key("home").qpos
+    st = time.time()
+    data = simulate()
+    times.append(time.time() - st)
+
+avg_time_ms = np.mean(times) * 1000
+std_time_ms = np.std(times) * 1000
+
+print(f"Average time: {avg_time_ms:.4f} +/- {std_time_ms:.4f} ms")
+
